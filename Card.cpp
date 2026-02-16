0fecdc0bc663195815c4160ca5e0b86a3da16b18
ColateAhax <nico.socas@gmail.com>
2026-02-16 16:23:59 -0500

    Rough Draft of Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -93,6 +93,128 @@ std::istream & operator>>(std::istream &is, Suit &suit) {
 
 /////////////// Write your implementation for Card below ///////////////
 
+Card::Card()
+  : rank(TWO), suit (SPADES)
+{}
+
+Card::Card(Rank rank_in, Suit suit_in)
+  : rank(rank_in), suit(suit_in)
+{}
+
+Rank Card::get_rank() const   {return rank;}
+
+Suit Card::get_suit() const   {return suit;}
+
+Suit Card::get_suit(Suit trump) const
+{
+  if (is_left_bower(trump)) return trump;
+  
+  return suit;
+}
+
+bool Card::is_trump(Suit trump) const
+{
+  if(suit == trump || is_left_bower(trump)) return true;
+  return false;
+}
+
+bool Card::is_face_or_ace() const   {return (rank >= 9 && rank <= 12);}
+
+bool Card::is_right_bower(Suit trump) const{ return (suit == trump && rank == JACK);}
+
+
+bool Card::is_left_bower(Suit trump) const///
+{
+  if (rank == JACK)
+  {
+    if (suit == Suit_next(trump)){
+      return true;
+    }
+}
+return false;
+}
+
+//EFFECTS Returns true if a is lower value than b.  Uses trump to determine
+// order, as described in the spec.
+
+bool Card_less(const Card &a, const Card &b, Suit trump) 
+//Finished this check if its right when ur free @nick
+{
+  if(a.is_trump(trump))
+  {
+    return false;
+  } 
+  else if (b.is_trump(trump))
+  {
+    return true;
+  } 
+  else 
+  { //if both trump or both not
+
+    //most valuable bower
+    if(a.is_right_bower(trump))   return false;
+    else if (b.is_right_bower(trump))   return true;
+
+    //second most valuable bower
+    else if (a.is_right_bower(trump))   return false;
+    else if (b.is_right_bower(trump))   return true;
+
+    //ranks are the same --> tiebreaking with suit
+    if (a.get_rank() == b.get_rank())
+    {
+      return (a.get_suit() < b.get_suit());
+    }
+    //checking rank
+    return (a.get_rank() < b.get_rank());
+
+  } 
+}
+
+//EFFECTS Returns true if a is lower value than b.  Uses both the trump suit
+//  and the suit led to determine order, as described in the spec.
+bool Card_less(const Card &a, const Card &b, const Card &led_card, Suit trump)
+{
+  if(a.is_trump(trump))
+  {
+    return false;
+  } 
+  else if (b.is_trump(trump))
+  {
+    return true;
+  } 
+  else 
+  { //if both trump or both not 
+
+    //most valuable bower
+    if(a.is_right_bower(trump))   return false;
+    else if (b.is_right_bower(trump))   return true;
+
+    //second most valuable bower
+    else if (a.is_right_bower(trump))   return false;
+    else if (b.is_right_bower(trump))   return true;
+
+    //ranks are the same --> tiebreaking with suit
+    if (a.get_rank() == b.get_rank())
+    {
+      return (a.get_suit() < b.get_suit());
+    }
+    //checking rank
+    return (a.get_rank() < b.get_rank());
+
+  } 
+}
+
+Suit Suit_next(Suit suit)
+{
+  Suit result;
+
+  if (suit == SPADES) result = CLUBS;
+  else if (suit == CLUBS) result = SPADES;
+  else if (suit == HEARTS) result = DIAMONDS;
+  else result = HEARTS;
+
+  return result;
+}
 
 // NOTE: We HIGHLY recommend you check out the operator overloading
 // tutorial in the project spec before implementing
@@ -105,3 +227,75 @@ std::istream & operator>>(std::istream &is, Suit &suit) {
 //   operator>=
 //   operator==
 //   operator!=
+bool operator == (const Card &card_left, const Card &card_right)
+{
+  //checks if both are trump or both are not
+  if (card_left.is_trump(card_left.get_suit()) != 
+      card_right.is_trump(card_right.get_suit())) return false;
+
+  //checks if its the same num
+  if (card_left.get_rank() != card_right.get_rank()) return false;
+
+  return true;
+}
+
+bool operator != (const Card &card_left, const Card &card_right)
+{
+  return !(card_left == card_right);
+}
+
+bool operator > (const Card &card_left, const Card &card_right)
+{
+  //makes sure they not equal to each other
+  if (card_left == card_right) return false;
+
+  //checks if one of them is the right bower
+  if (card_left.is_right_bower(card_left.get_suit())) return true;
+  if (card_right.is_right_bower(card_right.get_suit())) return false;
+
+  //checks if one of them is left boew
+  if (card_left.is_left_bower(card_left.get_suit())) return true;
+  if (card_right.is_left_bower(card_right.get_suit())) return false;
+
+  //checks if left card if trump and right isnt
+  if (card_left.is_trump(card_left.get_suit()) && 
+      !card_right.is_trump(card_right.get_suit())) return true;
+      
+  //checks if right card if trump and left isnt
+  if (card_left.is_trump(card_left.get_suit()) && 
+      !card_right.is_trump(card_right.get_suit())) return false;
+
+  return (card_left.get_rank() > card_right.get_rank());
+}
+
+bool operator < (const Card &card_left, const Card &card_right)
+{//GENUIUS
+  return !(card_left > card_right) && card_left != card_right;
+}
+
+bool operator <= (const Card &card_left, const Card &card_right)
+{//so stylish
+  return card_left < card_right || card_left == card_right;
+}
+
+bool operator >= (const Card &card_left, const Card &card_right)
+{
+  return (card_left > card_right || card_left == card_right);
+}
+
+//EFFECTS Prints Card to stream, for example "Two of Spades"
+std::ostream & operator<<(std::ostream &os, const Card &card)
+{
+  os << card.get_rank() << " of " << card.get_suit();
+  return os;
+}
+
+//EFFECTS Reads a Card from a stream in the format "Two of Spades"
+//NOTE The Card class declares this operator>> "friend" function,
+//     which means it is allowed to access card.rank and card.suit.
+std::istream & operator>>(std::istream &is, Card &card)
+{
+  std::string str;
+  is >> card.rank >> str >> card.suit;
+  return is;
+}
