e7fb69936486c1eb56d2cd2802acc401b23c1109
ColateAhax <nico.socas@gmail.com>
2026-02-18 02:53:19 -0500

    Added Lead Card and Play Card Functions and Tests

--- Player.cpp
+++ Player.cpp
@@ -2,22 +2,8 @@
 #include <cassert>
 #include <iostream>
 #include <array>
+#include <algorithm>
 using namespace std;
-
-//EFFECTS: Returns a pointer to a player with the given name and strategy
-//To create an object that won't go out of scope when the function returns,
-//use "return new Simple(name)" or "return new Human(name)"
-//Don't forget to call "delete" on each * after the game is over
-Player * Player_factory(const std::string &name, const std::string &strategy) 
-{
-  //
-}
-
-//EFFECTS: Prints player's name to os
-std::ostream & operator<<(std::ostream &os, const Player&p) 
-{
-  //
-}
 
 class SimplePlayer : public Player 
 {
@@ -26,7 +12,7 @@ class SimplePlayer : public Player
 
     const std::string & get_name() const override;
 
-    void add_card(const Card &c) override{hand.push_back(c);}
+    void add_card(const Card &c) override;
 
     bool make_trump(const Card &upcard, bool is_dealer,
                               int round, Suit &order_up_suit)const override;
@@ -54,7 +40,7 @@ const std::string & SimplePlayer::get_name() const
 ///EFFECTS  adds Card c to 's hand
 void SimplePlayer::add_card(const Card &c)
 {
-//
+  hand.push_back(c);
 }
 
 //REQUIRES round is 1 or 2
@@ -66,20 +52,23 @@ bool SimplePlayer::make_trump(const Card &upcard, bool is_dealer,
                         int round, Suit &order_up_suit) const
 {
   //RHEA
+  return false;
 }
 
 //REQUIRES  has at least one card
 //EFFECTS   adds one card to hand and removes one card from hand.
 void SimplePlayer::add_and_discard(const Card &upcard)
 {
-  hand.push_back(upcard);
   int comparing = 0;
-  for(int i = 0; i < hand.size(); i++){
-    if(Card_less(hand[i], hand[comparing], upcard.get_suit())){
+  for(int i = 0; i < hand.size(); i++)
+  {
+    if(Card_less(hand[i], hand[comparing], upcard.get_suit()))
+    {
       comparing = i;
     }
   }
   hand.erase(hand.begin() + comparing);
+  hand.push_back(upcard);
 }
 
 //REQUIRES  has at least one card
@@ -88,22 +77,66 @@ void SimplePlayer::add_and_discard(const Card &upcard)
 //  is removed the player's hand.
 Card SimplePlayer::lead_card(Suit trump)
 { //RHEA
-  vector<Card> temp;
+  bool has_all_trumps = true;
+  vector<int> index_non_trump;
   Card c;
 
   //checking if all of them are trump cards
-  for(int i = 0; i < hand.size(); i++){
-    if(!(hand[i].is_trump(trump))){
-      temp.push_back(hand[i]);
+  for(int i = 0; i < hand.size(); i++)
+  {
+    if(!(hand[i].is_trump(trump)))
+    {
+      has_all_trumps = false;
+      index_non_trump.push_back(i);
+    }
+  }
 
-      for(int j = 0; j < temp.size(); j++){
-        if(Card_less(c, hand[i], trump)){
-          c = hand[i];
-        }
+  if (has_all_trumps)
+  {
+    c = hand[0];
+    int indexBest = 0;
+    //cycles through the deck
+    for (int i = 1; i < hand.size(); i++)
+    {
+      Card current_card = hand[i];
+
+      //checks if the best card is less in value than the current
+      if (Card_less(c, current_card, trump))
+      {
+        c = current_card;
+        indexBest = i;
       }
     }
-      
+
+    //deletes the best card from hand then returns it
+    hand.erase(hand.begin() + indexBest);
   }
+
+  else
+  {
+    c = hand[index_non_trump[0]];
+    int indexBest = index_non_trump[0];
+    //cycles through the deck
+    for (int i = 1; i < hand.size(); i++)
+    {
+      //checks that current index is a non trump
+      if (find(index_non_trump.begin(), index_non_trump.end(), i)
+           == index_non_trump.end()) continue;
+
+      Card current_card = hand[i];
+
+      //checks if the best card is less in value than the current
+      if (Card_less(c, current_card, trump))
+      {
+        c = current_card;
+        indexBest = i;
+      }
+    }
+
+    //deletes the best card from hand then returns it
+    hand.erase(hand.begin() + indexBest);
+  }
+  return c;
 }
 
 
@@ -112,86 +145,167 @@ Card SimplePlayer::lead_card(Suit trump)
 //  The card is removed from the player's hand.
 Card SimplePlayer::play_card(const Card &led_card, Suit trump)
 {
-  //
+  //checks if it can follow suit
+  bool follow_suit = false;
+  Suit suit_led_card = led_card.get_suit(trump);
+  for (int i = 0; i < hand.size(); i++)
+  {
+    //automatically breaks it because you can follow suit
+    if(hand[i].get_suit(trump) == suit_led_card)
+    {
+      follow_suit = true;
+      break;
+    }
+  }
+
+  //if it can follow suit goes for best card
+  if (follow_suit)
+  {
+    int indexBest = 0;
+
+    //finds the first card that follows suit
+    for (int i = 0; i < hand.size(); i++)
+    {
+      if (hand[i].get_suit(trump) == suit_led_card)
+      {
+        indexBest = i;
+        break;
+      }
+    }
+    Card best_Card = hand[indexBest];
+
+    //cycles through the deck
+    for (int i = 1; i < hand.size(); i++)
+    {
+      Card current_card = hand[i];
+
+      //makes sure its in the suit of the led card
+      if (hand[i].get_suit(trump) != suit_led_card) continue;
+      //checks if the best card is less in value than the current
+      if (Card_less(best_Card, current_card, led_card, trump))
+      {
+        best_Card = current_card;
+        indexBest = i;
+      }
+    }
+
+    //deletes the best card from hand then returns it
+    hand.erase(hand.begin() + indexBest);
+    return best_Card;
+  }
+
+  //it cannot follow suit
+  else
+  {
+    Card worst_Card = hand[0];
+    int indexWorst = 0;
+
+    //cycles through the deck
+    for (int i = 1; i < hand.size(); i++)
+    {
+      Card current_card = hand[i];
+
+      //checks if the current card is less in value than worst
+      if (Card_less(current_card, worst_Card, led_card, trump))
+      {
+        worst_Card = current_card;
+        indexWorst = i;
+      }
+    }
+
+    //deletes the best card from hand then returns it
+    hand.erase(hand.begin() + indexWorst);
+    return worst_Card;
+  }
 }
 
 
 
 
 
+//-----------------------------------------------------
+
+//i commented everything down here so it would compile
+//you can uncomment everything from down here if you
+//wanna start on Human Player
+
+//-----------------------------------------------------
+
+
+
+// //for human player
+// class HumanPlayer : public Player 
+// {
+//   public:
+//     HumanPlayer(const std::string name) :name(name){};
+
+//     const std::string & get_name() const override;
+
+//     void add_card(const Card &c) override;
+//     bool make_trump(const Card &upcard, bool is_dealer,
+//                               int round, Suit &order_up_suit) const override;
+//     void add_and_discard(const Card &upcard) override;
+//     Card lead_card(Suit trump) override;
+//     Card play_card(const Card &led_card, Suit trump) override;
+//   private:
+//     std::string name;
+// };
+
+// HumanPlayer::HumanPlayer(const std::string name)
+// :name(name){};
+
+// const std::string & HumanPlayer::get_name() const
+// {
+//   return name;
+// }
+
+// //REQUIRES player has less than MAX_HAND_SIZE cards
+// ///EFFECTS  adds Card c to 's hand
+// void HumanPlayer::add_card(const Card &c)
+// {
+//   //
+// }
+
+// //REQUIRES round is 1 or 2
+// //MODIFIES order_up_suit
+// //EFFECTS If  wishes to order up a trump suit then return true and
+// //  change order_up_suit to desired suit.  If  wishes to pass, then do
+// //  not modify order_up_suit and return false.
+// bool HumanPlayer::make_trump(const Card &upcard, bool is_dealer,
+//                         int round, Suit &order_up_suit) const
+// {
+//   // RHEA
+// }
+
+// //REQUIRES  has at least one card
+// //EFFECTS   adds one card to hand and removes one card from hand.
+// void HumanPlayer::add_and_discard(const Card &upcard)
+// {
+//   //
+// }
+
+// //REQUIRES  has at least one card
+// //EFFECTS  Leads one Card from 's hand according to their strategy
+// //  "Lead" means to play the first Card in a trick.  The card
+// //  is removed the player's hand.
+// Card HumanPlayer::lead_card(Suit trump)
+// {
+//   // RHEA
+// }
+
+// //REQUIRES  has at least one card
+// //EFFECTS  Plays one Card from 's hand according to their strategy.
+// //  The card is removed from the player's hand.
+// Card HumanPlayer::play_card(const Card &led_card, Suit trump)
+// {
+//   //
+// }
 
 
 
 
 
 
-//for human player
-class HumanPlayer : public Player 
-{
-  public:
-    HumanPlayer(const std::string name) :name(name){};
-
-    const std::string & get_name() const override;
-
-    void add_card(const Card &c) override;
-    bool make_trump(const Card &upcard, bool is_dealer,
-                              int round, Suit &order_up_suit) const override;
-    void add_and_discard(const Card &upcard) override;
-    Card lead_card(Suit trump) override;
-    Card play_card(const Card &led_card, Suit trump) override;
-  private:
-    std::string name;
-};
-
-HumanPlayer::HumanPlayer(const std::string name)
-:name(name){};
-
-const std::string & HumanPlayer::get_name() const
-{
-  return name;
-}
-
-//REQUIRES player has less than MAX_HAND_SIZE cards
-///EFFECTS  adds Card c to 's hand
-void HumanPlayer::add_card(const Card &c)
-{
-  //
-}
-
-//REQUIRES round is 1 or 2
-//MODIFIES order_up_suit
-//EFFECTS If  wishes to order up a trump suit then return true and
-//  change order_up_suit to desired suit.  If  wishes to pass, then do
-//  not modify order_up_suit and return false.
-bool HumanPlayer::make_trump(const Card &upcard, bool is_dealer,
-                        int round, Suit &order_up_suit) const
-{
-  // RHEA
-}
-
-//REQUIRES  has at least one card
-//EFFECTS   adds one card to hand and removes one card from hand.
-void HumanPlayer::add_and_discard(const Card &upcard)
-{
-  //
-}
-
-//REQUIRES  has at least one card
-//EFFECTS  Leads one Card from 's hand according to their strategy
-//  "Lead" means to play the first Card in a trick.  The card
-//  is removed the player's hand.
-Card HumanPlayer::lead_card(Suit trump)
-{
-  // RHEA
-}
-
-//REQUIRES  has at least one card
-//EFFECTS  Plays one Card from 's hand according to their strategy.
-//  The card is removed from the player's hand.
-Card HumanPlayer::play_card(const Card &led_card, Suit trump)
-{
-  //
-}
 
 
 
@@ -200,6 +314,27 @@ Card HumanPlayer::play_card(const Card &led_card, Suit trump)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EFFECTS: Returns a pointer to a player with the given name and strategy
+//To create an object that won't go out of scope when the function returns,
+//use "return new Simple(name)" or "return new Human(name)"
+//Don't forget to call "delete" on each * after the game is over
 Player * Player_factory(const std::string &name, 
                         const std::string &strategy) 
 {
@@ -210,12 +345,20 @@ Player * Player_factory(const std::string &name,
     // The "new" keyword dynamically allocates an object.
     return new SimplePlayer(name);
   }
-  // Repeat for each other type of 
-  if (strategy == "Human") 
-  {
-    // The "new" keyword dynamically allocates an object.
-    return new HumanPlayer(name);
-  }
+  // // Repeat for each other type of Player 
+  // if (strategy == "Human") 
+  // {
+  //   // The "new" keyword dynamically allocates an object.
+  //   return new HumanPlayer(name);
+  // }
   // Invalid strategy if we get here
+  assert(false);
   return nullptr;
+}
+
+//EFFECTS: Prints player's name to os
+std::ostream & operator<<(std::ostream &os, const Player&p) 
+{
+  os << p.get_name();
+  return os;
 }
--- Player_tests.cpp
+++ Player_tests.cpp
@@ -5,13 +5,201 @@
 
 using namespace std;
 
-TEST(test_player_get_name) {
+TEST(test_player_get_name) 
+{
     Player * alice = Player_factory("Alice", "Simple");
     ASSERT_EQUAL("Alice", alice->get_name());
 
     delete alice;
 }
-
 // Add more tests here
+
+
+
+//-------------------------------------------------
+//SIMPLE PLAYER TESTS
+//-------------------------------------------------
+
+
+// //EFFECTS returns player's name
+// virtual const std::string & get_name() const = 0;
+TEST(test_simple_player_get_name)
+{
+    Player * simple_Player = Player_factory("Simple Play", "Simple");
+    ASSERT_EQUAL("Simple Play", simple_Player->get_name());
+
+    delete simple_Player;
+}
+
+// //REQUIRES player has less than MAX_HAND_SIZE cards
+// //EFFECTS  adds Card c to Player's hand
+// virtual void add_card(const Card &c) = 0;
+TEST(test_simple_player_add_card)
+{
+    Player * simple_Player = Player_factory("Simple Play", "Simple");
+    Card card1(JACK, HEARTS);
+    Card card2(QUEEN, HEARTS);
+
+    simple_Player->add_card(card1);
+    simple_Player->add_card(card2);
+
+    //in this case card 2 is best card to play
+    Card temp(TEN, SPADES);
+    ASSERT_TRUE(card2 == simple_Player->play_card(temp, HEARTS));
+
+    delete simple_Player;
+}
+
+// //REQUIRES round is 1 or 2
+// //MODIFIES order_up_suit
+// //EFFECTS If Player wishes to order up a trump suit then return true and
+// //  change order_up_suit to desired suit.  If Player wishes to pass, then do
+// //  not modify order_up_suit and return false.
+// virtual bool make_trump(const Card &upcard, bool is_dealer,
+//                         int round, Suit &order_up_suit) const = 0;
+TEST(test_simple_player_make_trump)
+{
+    Player * simple_Player = Player_factory("Simple Play", "Simple");
+
+    delete simple_Player;
+}
+
+// //REQUIRES Player has at least one card
+// //EFFECTS  Player adds one card to hand and removes one card from hand.
+// virtual void add_and_discard(const Card &upcard) = 0;
+TEST(test_simple_player_add_and_discard)
+{
+    Player * simple_Player = Player_factory("Simple Play", "Simple");
+
+    delete simple_Player;
+}
+
+// //REQUIRES Player has at least one card
+// //EFFECTS  Leads one Card from Player's hand according to their strategy
+// //  "Lead" means to play the first Card in a trick.  The card
+// //  is removed the player's hand.
+// virtual Card lead_card(Suit trump) = 0;
+TEST(test_simple_player_lead_card)
+{
+    Player * simple_Player = Player_factory("Simple Play", "Simple");
+    Suit trump = HEARTS;
+
+    simple_Player->add_card(Card(JACK, trump)); //right bower
+    simple_Player->add_card(Card(JACK, DIAMONDS)); //left bower
+    simple_Player->add_card(Card(TEN, trump)); //trump
+    simple_Player->add_card(Card(NINE, DIAMONDS)); //non trump
+    simple_Player->add_card(Card(QUEEN, CLUBS)); //non trump
+
+    //since it returns highest non trump card then it should return queen of clubs
+    ASSERT_TRUE(simple_Player->lead_card(trump) == Card(QUEEN, CLUBS));
+    delete simple_Player;
+
+
+    Player * simple_Player2 = Player_factory("Simple Play", "Simple");
+
+    simple_Player2->add_card(Card(JACK, trump)); //right bower
+    simple_Player2->add_card(Card(JACK, DIAMONDS)); //left bower
+    simple_Player2->add_card(Card(TEN, trump)); //trump
+    simple_Player2->add_card(Card(NINE, trump)); //trump
+    simple_Player2->add_card(Card(QUEEN, trump)); //trump
+    //since player has all trumps it should return right bower
+    ASSERT_TRUE(simple_Player2->lead_card(trump) == Card(JACK, trump));
+    delete simple_Player2;
+}
+
+// //REQUIRES Player has at least one card
+// //EFFECTS  Plays one Card from Player's hand according to their strategy.
+// //  The card is removed from the player's hand.
+// virtual Card play_card(const Card &led_card, Suit trump) = 0;
+TEST(test_simple_player_play_card)
+{
+    Player * simple_Player = Player_factory("Simple Play", "Simple");
+    Suit trump = HEARTS;
+    Card led_card(TEN, SPADES);
+
+    simple_Player->add_card(Card(JACK, trump)); //right bower
+    simple_Player->add_card(Card(JACK, DIAMONDS)); //left bower
+    simple_Player->add_card(Card(QUEEN, SPADES)); //led suit
+    simple_Player->add_card(Card(NINE, SPADES)); //led suit
+    simple_Player->add_card(Card(QUEEN, CLUBS)); //random
+
+    //since player can follow suit
+    //it should return Queen of Spades
+    ASSERT_TRUE(simple_Player->play_card(led_card, trump) == Card(QUEEN, SPADES));
+    delete simple_Player;
+
+
+    Player * simple_Player2 = Player_factory("Simple Play", "Simple");
+
+    simple_Player2->add_card(Card(JACK, trump)); //right bower
+    simple_Player2->add_card(Card(JACK, DIAMONDS)); //left bower
+    simple_Player2->add_card(Card(TEN, trump)); //trump
+    simple_Player2->add_card(Card(NINE, trump)); //trump
+    simple_Player2->add_card(Card(QUEEN, trump)); //trump
+    //since player cannot follow suit it plays worst hand
+    //which is a Nine of Hearts
+    ASSERT_TRUE(simple_Player2->play_card(led_card, trump) == Card(NINE, trump));
+    delete simple_Player2;
+}
+
+
+
+//-------------------------------------------------
+//HUMAN PLAYER TESTS
+//-------------------------------------------------
+
+// //EFFECTS returns player's name
+// virtual const std::string & get_name() const = 0;
+TEST(test_human_player_get_name)
+{
+    //
+}
+
+// //REQUIRES player has less than MAX_HAND_SIZE cards
+// //EFFECTS  adds Card c to Player's hand
+// virtual void add_card(const Card &c) = 0;
+TEST(test_human_player_add_card)
+{
+    //
+}
+
+// //REQUIRES round is 1 or 2
+// //MODIFIES order_up_suit
+// //EFFECTS If Player wishes to order up a trump suit then return true and
+// //  change order_up_suit to desired suit.  If Player wishes to pass, then do
+// //  not modify order_up_suit and return false.
+// virtual bool make_trump(const Card &upcard, bool is_dealer,
+//                         int round, Suit &order_up_suit) const = 0;
+TEST(test_human_player_make_trump)
+{
+    //
+}
+
+// //REQUIRES Player has at least one card
+// //EFFECTS  Player adds one card to hand and removes one card from hand.
+// virtual void add_and_discard(const Card &upcard) = 0;
+TEST(test_human_player_add_and_discard)
+{
+    //
+}
+
+// //REQUIRES Player has at least one card
+// //EFFECTS  Leads one Card from Player's hand according to their strategy
+// //  "Lead" means to play the first Card in a trick.  The card
+// //  is removed the player's hand.
+// virtual Card lead_card(Suit trump) = 0;
+TEST(test_human_player_lead_card)
+{
+    //
+}
+
+// //REQUIRES Player has at least one card
+// //EFFECTS  Plays one Card from Player's hand according to their strategy.
+// //  The card is removed from the player's hand.
+// virtual Card play_card(const Card &led_card, Suit trump) = 0;
+TEST(test_human_player_play_card)
+{
+    //
+}
 
 TEST_MAIN()
